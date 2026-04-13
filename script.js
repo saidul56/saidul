83c08102d75c9c65982b28f9eb6aa621cabd3ed5
Saidul Islam <iamsaidul23@gmail.com>
2026-04-13 23:07:45 +0600

    Add showSection function to manage active sections

---
+++ script.js
@@ -0,0 +1,6 @@
+function showSection(id) {
+    let sections = document.querySelectorAll("section");
+    sections.forEach(sec => sec.classList.remove("active"));
+
+    document.getElementById(id).classList.add("active");
+}
